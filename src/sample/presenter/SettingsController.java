5cdb7d5f2f307c3b3a8dd12644c89f9ed6282335
Pavlo99 <50137022+Pavlo99@users.noreply.github.com>
2021-01-09 16:27:49 +0200

    Some changes(Pavlo)

--- src/sample/presenter/SettingsController.java
+++ src/sample/presenter/SettingsController.java
@@ -4,6 +4,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BackgroundImage;
 
 public class SettingsController {
 
@@ -16,8 +18,12 @@ public class SettingsController {
     @FXML
     private Button save_button;
 
+    private final Image backgroundImage = new Image("sample/images/main_background.png");
+    private final BackgroundImage background = new BackgroundImage(backgroundImage, null, null, null, null);
+
+
     @FXML
     void initialize() {
-
+        assert save_button != null : "fx:id=\"save_button\" was not injected: check your FXML file 'settings.fxml'.";
     }
 }
