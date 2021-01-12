9bef2b7a53a78163d61a232cdc3a10ab8cb180af
Andriichello <andriichello@gmail.com>
2021-01-12 11:51:51 +0200

    ElevatorScene methods updated to match floor numeration

---
+++ src/data/elevator/strategy/StopByElevatorStrategy.java
@@ -0,0 +1,31 @@
+package data.elevator.strategy;
+
+import data.elevator.Elevator;
+
+public class StopByElevatorStrategy implements ElevatorStrategy {
+
+    @Override
+    public boolean hasWhereToGo(Elevator elevator) {
+        return !elevator.getConsumers().isEmpty() || !elevator.getCalledFloors().isEmpty();
+    }
+
+    @Override
+    public int resolveFloorToGo(Elevator elevator) {
+        if (!hasWhereToGo(elevator)) throw new IllegalStateException(
+                "Cannot resolve elevator floor to go because there are no consumers and no called floors"
+        );
+
+        // Check consumers inside first
+        if (!elevator.getConsumers().isEmpty()) {
+            return elevator.getConsumers().get(0).destinationFloor();
+        }
+
+        // Check called floors otherwise
+        if (!elevator.getCalledFloors().isEmpty()) {
+            return elevator.getCalledFloors().stream().findFirst().get();
+        }
+
+        throw new IllegalStateException("Elevator strategy felt into error");
+    }
+
+}
